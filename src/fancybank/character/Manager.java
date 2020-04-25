1787bfd424ab154cb03324eb96dae6f15af74390
Jessy <jinshuy@bu.edu>
2020-04-25 18:24:00 -0400

    work on login

---
+++ src/fancybank/character/Manager.java
@@ -0,0 +1,12 @@
+package fancybank.character;
+
+public class Manager extends Character{
+
+    
+
+    public Manager(String name, String accountName, String pwd) {
+        super(name, accountName, pwd);
+        // TODO Auto-generated constructor stub
+    }
+
+}
