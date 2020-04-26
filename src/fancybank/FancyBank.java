3751c64d33bc48c45363f34c72eebb44b65db875
Phumin Walaipatchara <phuminw@bu.edu>
2020-04-26 14:49:50 +0700

    Import accounts

--- src/fancybank/FancyBank.java
+++ src/fancybank/FancyBank.java
@@ -1,9 +1,16 @@
 package fancybank;
 
+import java.util.List;
+
+import fancybank.account.CheckingAccount;
+import fancybank.account.SavingAccount;
+import fancybank.account.SecuritiesAccount;
+
 /**
  * Main program for the bank
  */
 public class FancyBank {
+    /* DEBUG flag: Enable more options and settings for debugging */
     public static final boolean DEBUG = true;
 
     private List<SavingAccount> savings;
