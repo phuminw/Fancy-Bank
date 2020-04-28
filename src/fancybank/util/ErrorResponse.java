0b0b6dd5b06df9f6e161e458157eae8079b99143
Yizhou Mao <uohziyoam@gmail.com>
2020-04-27 23:52:29 -0400

    add response

---
+++ src/fancybank/util/ErrorResponse.java
@@ -0,0 +1,11 @@
+public class ErrorResponse {
+    public String res = new String();
+
+    public ErrorResponse(final String res) {
+        this.res = res;
+
+    }
+
+    public ErrorResponse() {
+    }
+}
