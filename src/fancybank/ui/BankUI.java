3fe0e8204e51ab42f72e8338f7f1598cbd84dbef
Phumin Walaipatchara <phuminw@bu.edu>
2020-05-05 10:42:44 +0700

    Remove final

--- src/fancybank/ui/BankUI.java
+++ src/fancybank/ui/BankUI.java
@@ -6,7 +6,7 @@ import fancybank.FancyBank;
 import fancybank.util.*;
 
 public class BankUI extends JFrame {
-    private final FancyBank fancybank; // Init the band instance
+    private static FancyBank fancybank; // Init the band instance
     private final WelcomePage welcomePage = new WelcomePage(this);
     private final UserLoginPanel userLoginPage = new UserLoginPanel(this);
     private final UserDetailPanel userPage = new UserDetailPanel(this);
@@ -15,10 +15,8 @@ public class BankUI extends JFrame {
     private final SecurityInfoPanel securitiesInfoPanel = new SecurityInfoPanel();
     private final SecurityAccountInfoPanel securitiesAccountInfoPanel = new SecurityAccountInfoPanel();
 
-
-
     BankUI(FancyBank fancybank) {
-        this.fancybank = fancybank;
+        BankUI.fancybank = fancybank;
         add(welcomePage);
         setTitle("Fancy Bank");
         setSize(600, 300);
